45c729e5feaf2a03450accd1b4e2cc04073aa9b7
s1115351 <s111351@student.hsleiden.nl>
2021-04-12 15:54:42 +0200

    Styling aangepast, allemaal gelijke styling nu. Afbeelding mist nog, gepusht wegens kans op crashen van computer

--- Laravel/Tropos/resources/views/humidity.blade.php
+++ Laravel/Tropos/resources/views/humidity.blade.php
@@ -1,38 +1,54 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Luchtvochtigheid</title>
-    <link rel="stylesheet" href="/css/humidity.css">
+<link rel="stylesheet" href="/css/humidity.css">
+@extends("baseview")
+
+@section('title')
+Luchtvochtigheid
+@endsection
+
+@section('body')
+
     <script>
         //javaScript
         window.onload=function(){
 
+            laatsteWaarde();
             trim();
+
+            function laatsteWaarde(){
+                hum = String({{$hum->luchtvochtigheid}})
+                document.getElementById('-js--laatsteWaarde').innerHTML="Op dit moment is het " + hum + "%"
+            }
 
             function trim(){
                 avghum = String({{$avghum}})
-                document.getElementById('-js--gemiddelde').innerHTML="gemiddelde: " + avghum
+                document.getElementById('-js--gemiddelde').innerHTML="Gemiddelde: " + avghum + "%"
             }
         }
     </script>
-</head>
 
-<body>
+    <header>
+        <nav class="nav">
+            <a class="nav_link navlinknotHover navlink--img" href="/home"><figure><img class="navlink_img" src="/img/troposLogo.png" alt="logo van Tropos"></figure></a>
+            <a class="nav__link" href="/home">Home</a>
+            <a class="nav__link" href="/temperatuur">Temperatuur</a>
+            <a class="nav__link navlink_current" href="/humidity">Luchtvochtigheid</a>
+            <a class="nav__link" href="/airpressure">Luchtdruk</a>
+            <a class="nav__link" href="#">Stof</a>
+            <a class="nav__link" href="/monoxide">Koolmonoxide</a>
+        </nav>  
+    </header>
+
     <div class="container">
         <ul class="container__list">
             <li class="container__list__top">
                 <h1>Luchtvochtigheid</h1>
             </li>
-            <li class="container__list__item">
-                <h2>{{$hum->luchtvochtigheid}}</h2>
+            <li class="container__list__hum">
+                <h2 id="-js--laatsteWaarde">{{$hum->luchtvochtigheid}}</h2>
             </li>
-            <li class="container__list__item">
+            <li class="container__list__avg">
                 <h2 id="-js--gemiddelde">{{$avghum}}</h2>
             </li>
         </ul>
     </div>
-</body>
-</html>
+ @endsection
