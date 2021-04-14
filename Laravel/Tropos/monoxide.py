d445088fcd92b3af0989a84ea3475046b12ce154
s1115351 <s1115351@student.hsleiden.nl>
2021-04-14 11:31:20 +0200

    merge conflicts problemen proberen op te lossen, chaos in de git messages

---
+++ Laravel/Tropos/monoxide.py
@@ -0,0 +1,36 @@
+import mysql.connector
+import time
+
+import serial
+import os
+
+mydb = mysql.connector.connect(
+    host = "localhost",
+    user = "testuser",
+    passwd = "testuser",
+    database = "ipmedt5"
+)
+port = serial.Serial("/dev/ttyUSB0", baudrate=9600, timeout=3.0)
+mycursor = mydb.cursor()
+
+while True:
+    mycursor.execute("SELECT * FROM buzzers;")
+    for x in mycursor:
+        print(x[3])
+    if x[3] == 'aan':
+        print(x[3])
+        port.write("b0")    #buzzer off later (m = motor)
+    # else:
+    #     port.write("b0")    #buzzer off
+
+
+    rcv = port.readline().strip()
+    mycursor.execute("UPDATE carbon_monoxide SET amount = " + rcv + ";")
+    print(rcv)
+
+    time.sleep(1)
+    mydb.commit()
+
+mydb.close()
+
+
--- Laravel/Tropos/resources/views/default.blade.php
+++ Laravel/Tropos/resources/views/default.blade.php
@@ -4,11 +4,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/css/humidity.css">
-    <link rel="stylesheet" href="/css/main.css">
-    <title>@yield('title')</title>
+    <link rel="stylesheet" href="./css/main.css">
+    <script type="text/javascript" src="./js/main.js"></script>
+
+    
+    <title>Stof</title>
 </head>
     <body>
-         @yield("body")
+        @yield('content')
     </body>
 </html>
